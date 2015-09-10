be734e28a5b6422bb97e516947a9982ff0291876
Rich Nelson <speeddemon974@gmail.com>
2015-09-10 01:08:27 -0400

    added Test Mode

--- rt-movie-renamer.py
+++ rt-movie-renamer.py
@@ -1,15 +1,19 @@
 # deal with srt (subtitles) if moving files
-
 import requests, os, bs4, re, shutil
 
-SIMPLEREG = re.compile('(.*?)(?:[\[\(])*([0-9]{4})(.*)')
-CRITICREG = re.compile('class="tMeterScore">([0-9]{2})')
-VIDEO_FN_EXTS = set('3g2 3gp asf asx avi flv m4v mkv mov mp4 mpg rm swf vob wmv'.split())
-DISALLOWED_CHARS = set('\/:*?"<>')
+MOVE_FILES = True # True/False Move files to base directory & delete old folders.
+TEST_MODE = False # True/False Test mode will print old/new filenames instead of renaming and moving the files.
 
-dud = found = 0
+file_name_regex = re.compile('(.*?)(?:[\[\(])*([0-9]{4})(.*)')
+video_extensions = set('3g2 3gp asf asx avi flv m4v mkv mov mp4 mpg rm swf vob wmv'.split())
+
+base_dir = os.getcwd()
+not_found = found = 0
 
 def check_url():
+    """ Tests for a url match
+    Rotten Tomatoes URLs can be /m/movie_title or /m/movie_title_year or /m/movie_title-year
+    """
     url_base = 'http://www.rottentomatoes.com/m/' + underscore_title
     tomato_url = url_base + '_' + movie_year + '/'
     tomato_html = requests.get(tomato_url)
@@ -27,6 +31,8 @@ def check_url():
     
 
 def get_rating(tomato_html):
+    """ Get's the Critic and Audience Ratings using Beatiful soup
+    """
     rating_critic = rating_audience = 'na'
     bs4_obj = bs4.BeautifulSoup(tomato_html.text, "html.parser")
     for x, row in enumerate(bs4_obj.find_all('span',attrs={"class" : "meter-value"})):
@@ -39,45 +45,49 @@ def get_rating(tomato_html):
 
 
 
-current_dir = os.getcwd()
 
-for subfoldername, subfolder, filenames in os.walk(current_dir): #loop all files in directory
-    #head, tail = os.path.split(subfoldername)       #splits path from last folder name
-    
-    for filename in filenames:                  #loops through file names in directory
-        file_ext = filename[filename.rfind(".")+1:]
-
-        if file_ext in VIDEO_FN_EXTS:       #check if it's a video file
-            movie = SIMPLEREG.search(filename)
-            if movie and "sample" not in movie.group(3):
+for subfoldername, subfolder, filenames in os.walk(base_dir): 
+    for filename in filenames:                  
+        file_extention = filename[filename.rfind(".")+1:]
+        if file_extention in video_extensions:      
+            movie = file_name_regex.search(filename)
+            if movie and "sample" not in movie.group(3):        #ignores video sample files
                 movie_title = movie.group(1).replace('.',' ').strip().lower()
                 underscore_title = movie_title.replace(' ','_')
                 movie_year = movie.group(2)
-
                 tomato_html = check_url()
-                if tomato_html is None and "the" in movie_title:# and "the" in movie_title:
+                if tomato_html is None and "the" in movie_title:        # If RT URL has not been found, and the title contains a 'the', this strips the 'the' and tries again
                     underscore_title = underscore_title.replace('the_','')
                     tomato_html = check_url()
 
                 if (tomato_html):
                     rating_critic, rating_audience = get_rating(tomato_html)
-                    print(movie_title.title() + " (" + movie_year + ") C:" + rating_critic + " A:" + rating_audience)
-                    new_path =  current_dir + "\\" + movie_title.title() + " (" + movie_year + ") C:" + rating_critic + " A:" + rating_audience + "." + file_ext
+                    new_name =  "\\" + movie_title.title() + " (" + movie_year + ") C-" + rating_critic + " A-" + rating_audience + "." + file_extention
+                    print(new_name)
                     found += 1
-                    #print(movie_date, movie_title)
                 else:
                     print('-',movie_title,movie_year,"- Valid URL Not Found")
-                    new_path =  current_dir + "\\" + filename
-                    dud += 1
-                old_path = subfoldername+ "\\" + filename
-                #print(old_path)
-                #print(new_path)
-                shutil.move(old_path, new_path)
-                if subfoldername != current_dir:
-                    #print('delete', )
-                    shutil.rmtree(subfoldername)
-                
-                    
-print('not found:',dud)              
+                    if MOVE_FILES:
+                        new_path =  base_dir + "\\" + filename
+                    not_found += 1
 
+                old_path = subfoldername+ "\\" + filename
+                if MOVE_FILES:
+                    new_path = base_dir + new_name
+                elif not MOVE_FILES:
+                    new_path = subfoldername + new_name
+                if TEST_MODE:
+                    print('old path:',old_path)
+                    print('new path:',new_path)
+                elif not TEST_MODE:
+                    shutil.move(old_path, new_path)
+                if subfoldername != base_dir and MOVE_FILES:
+                    if TEST_MODE:
+                        print('delete:  ', subfoldername)
+                    elif not TEST_MODE:
+                        shutil.rmtree(subfoldername)
+                
+
+print('')                  
+print('found:',found,'not found:',not_found)              
 input('\nPress Enter to close')
